0a7462dd498403271f1cecd7e0c60358f49f01db
Mordecai <chingolo265@gmail.com>
2024-08-25 19:52:36 +0200

    first commit

---
+++ linearSearch.js
@@ -0,0 +1,15 @@
+// Linear Search Algo
+
+const numbersArr = [3, 5, 11, 12, 17, 24];
+
+ function linearSearch (arr, x){
+    let len = arr.length;
+
+    for(let i = 0; i < len; i++){
+        if(x == arr[i]) return i;
+    }
+
+    return -1
+ }
+
+ console.log(linearSearch(numbersArr, 5));
