646e042b305e615828634b3ab743fee1ce532052
Mordecai <chingolo265@gmail.com>
2024-08-25 22:01:02 +0200

    changes

--- linearSearch.js
+++ linearSearch.js
@@ -2,14 +2,51 @@
 
 const numbersArr = [3, 5, 11, 12, 17, 24];
 
- function linearSearch (arr, x){
+//  function linearSearch (arr, x){
+//     let len = arr.length;
+
+//     for(let i = 0; i < len; i++){
+//         if(x == arr[i]) return i;
+//     }
+
+//     return -1
+//  }
+
+//  console.log(linearSearch(numbersArr, 5));
+
+
+function linearSearch (arr, x){
     let len = arr.length;
 
     for(let i = 0; i < len; i++){
         if(x == arr[i]) return i;
     }
 
-    return -1
- }
+    return 'not found'
+}
 
- console.log(linearSearch(numbersArr, 5));
+console.log(linearSearch(numbersArr, 12));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
